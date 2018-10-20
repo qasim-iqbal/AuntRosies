7f7552425fc668c0f9a1322a0128f392a3e543f8
Qasim Iqbal <Qasimighumman@hotmail.com>
2018-10-20 14:14:26 -0400

    added the mobile app project in

---
+++ Rosies_pie_shared/Rosies_pie_shared/Rosies_pie_shared/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Rosies_pie_shared.Services;
+using ZXing.Net.Mobile.Forms;
+
+namespace Rosies_pie_shared
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnScan_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var scanner = new ZXingScannerPage();
+                await Navigation.PushAsync(scanner);
+                scanner.OnScanResult += (result) =>
+                {
+                    Device.BeginInvokeOnMainThread(async () =>
+                    {
+                        await Navigation.PopAsync();
+                        lblResult.Text = result.Text;
+                    });
+                };
+
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ Rosies_pie_shared/Rosies_pie_shared/Rosies_pie_shared/Services/IQrScanningServices.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rosies_pie_shared.Services
+{
+    public interface IQrScanningServices
+    {
+        Task<string> ScanAsync();
+
+    }
+}
